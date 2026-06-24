81db45c4913b8d4a11be71b4c9f5030e943825b0
toro23028-cyber <toro23028@gmail.com>
2026-06-24 03:36:45 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCt0t7IvYYPMXTfXB1zZ6AB4Na9JpL50EQ",
+  apiKey: "SUA_CHAVE",
   authDomain: "tvweb-71058.firebaseapp.com",
   projectId: "tvweb-71058",
-  storageBucket: "tvweb-71058.firebasestorage.app",
-  messagingSenderId: "325260348531",
-  appId: "1:325260348531:web:45e9468625cbe3b8d4ae06"
+  storageBucket: "tvweb-71058.appspot.com",
+  messagingSenderId: "SUA_ID",
+  appId: "SUA_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
